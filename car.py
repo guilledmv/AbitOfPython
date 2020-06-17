18c44315012fb8a39514e74b460f60c342c4ea85
guilledmv <guillemv98@hotmail.com>
2020-06-17 12:43:43 +0200

    Telling about proccess of my methods in class

--- car.py
+++ car.py
@@ -1,18 +1,18 @@
 class Car():
-    
+    # We create a build method
     def __init__(self):
         self.__brand = "bmw"
         self.__wheels = 4
         self.__cv = 190
         self.__started = False
-
+    # Method which returns if this car was started
     def start(self,aux):
         self.__started = aux
         if(self.__started):
             return True
         else:
             return False
-    
+    # Method which prints an attributes of our object
     def state(self,aux22):
         print("Brand's car is : ",self.__brand," quantity of wheels : ",self.__wheels," Started : ",self.start(aux22))
 
