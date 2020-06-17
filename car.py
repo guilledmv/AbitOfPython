dc2049e69f50053bda3018504d2c011391e044fa
guilledmv <guillemv98@hotmail.com>
2020-06-17 12:35:38 +0200

    My first class in python!

---
+++ car.py
@@ -0,0 +1,26 @@
+class Car():
+    
+    def __init__(self):
+        self.__brand = "bmw"
+        self.__wheels = 4
+        self.__cv = 190
+        self.__started = False
+
+    def start(self,aux):
+        self.__started = aux
+        if(self.__started):
+            return True
+        else:
+            return False
+    
+    def state(self,aux22):
+        print("Brand's car is : ",self.__brand," quantity of wheels : ",self.__wheels," Started : ",self.start(aux22))
+
+car1 = Car()
+aux2 = car1.start(True)
+car1.state(aux2)
+
+print("----------- We are gonna create another car-------------")
+car2 = Car()
+aux3 = car2.start(False)
+car2.state(aux3)
